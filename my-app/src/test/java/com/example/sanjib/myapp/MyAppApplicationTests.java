7fbbb707e68dcf603df244037d67c6fdb7e6d018
sanjib-koley <89680507+sanjib-koley@users.noreply.github.com>
2024-05-13 16:57:56 +0530

    Add files via upload

---
+++ my-app/src/test/java/com/example/sanjib/myapp/MyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sanjib.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
